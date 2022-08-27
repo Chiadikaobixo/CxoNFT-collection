997332db565d0d78ee68101445fe21cab61af089
chiadikaobixo <chiadikaobixo@gmail.com>
2022-08-27 02:52:22 -0500

    update: deployed contract

---
+++ constant/index.js
@@ -0,0 +1,7 @@
+METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+// Address of the whitelist contract that i deployed @
+// https://github.com/Chiadikaobixo/my-solidity/tree/master/whitelist
+WHITELIST_CONTRACT_ADDRESS = "0x4b5a54b2EbC2247B1B397346fFd705dDc0f9741f";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
